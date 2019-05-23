d4a6508dcd1386006d90ce1c0f4403089db06536
raechel <odom.raechel@gmail.com>
2019-05-23 16:49:43 -0600

    enzyme installed, 6 passing tests now

---
+++ src/reducers/__tests__/loadingReducer.js
@@ -0,0 +1,19 @@
+import { isLoading } from '../loadingReducer';
+import * as actions from '../../actions';
+
+describe('isLoading', () => {
+
+  it('should return the initial state', ()=> {
+    const expected = false;
+
+    const result = isLoading(undefined, []);
+    expect(result).toEqual(expected);
+  });
+
+  it('should return a truthy value in case of loading', () => {
+    const expected = true;
+    const result = isLoading(undefined, actions.isLoading);
+
+    expect(result).toEqual(expected);
+  })
+})
