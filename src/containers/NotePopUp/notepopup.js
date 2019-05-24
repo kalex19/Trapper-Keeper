d07bd7f85ca46270f29062b1c851cb3006d47c99
Saad Baradan <saadbaradan@gmail.com>
2019-05-24 10:56:14 -0600

    Add functionality that lists tasks on note card and saves note card as object to redux
    Co-authored-by: Raechel Odom<odom.raechel@gmail.com>

--- src/containers/NotePopUp/notepopup.js
+++ src/containers/NotePopUp/notepopup.js
@@ -29,44 +29,51 @@ export class notepopup extends Component {
 		})
 	};
 
-	// saveNote = () => {
-	// 	const { title, tasks } = this.state;
-	// 	const newNote = {
-	// 		id: Date.now(),
-	// 		title,
-  //     tasks,
-  //     completed: false
-	// 	}
-	// }
-
-	handleEnter = e => {
-    if(e.key === 'Enter' && !this.task === ''){
-    const  newTask = {
+	addTask = e => {
+		e.preventDefault();
+    if(this.task !== '') {
+    let newTask = {
         id: Date.now(),
         task: this.state.task,
         completed: false
-      }
+			}
       this.setState({
         tasks: [...this.state.tasks, newTask]
-      })
+			})
+			this.setState({task: ''})
     } 
-  };
+	};
+	
+	deleteTask = id => {
+		let tasks = this.state.tasks.filter(task => task.id !== id)
+		this.setState({tasks})
+	}
 
 	render() {
+		let input = <input type='text'/>;
 		return (
 			<form className='note-pop-up'>
 				<input value={this.state.title} placeholder="Title" name="title" type="text" onChange={this.handleChange} />
-				<input
-					value={this.state.task}
-					placeholder="List Item"
-					name="task"
-					type="text"
-					onChange={this.handleChange} 
-					onKeyPress={this.handleEnter}
-			/>
-			<Link exact path='/'>
+				<section className='task-handler'>
+					<button onClick={this.addTask} className='add-task'><i className="fas fa-plus"></i></button>
+					<input
+						value={this.state.task}
+						placeholder="List Item"
+						name="task"
+						type="text"
+						onChange={this.handleChange} 
+					/>
+					<button onClick={this.deleteTask} className='delete-task'><i className="fas fa-minus"></i></button>
+					{this.state.tasks.map(task => {
+						return <p contentEditable='true'><i class="far fa-square"></i>{task.task}</p>
+					})}
+				</section>
+			<Link to='/'>
 			<button onClick={this.submitForm}>Save</button>
 			</Link>
+			<button className="delete">
+					<i className="fas fa-trash-alt" />
+			</button>
 		</form>
 		);
 	}
