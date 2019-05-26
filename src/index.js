0d57ca8301c0b5ede54dc29e970be9fed1abcb4a
Katherine Lewis <kale8432@colorado.edu>
2019-05-25 19:45:29 -0600

    Refactored scss, cleaned up index.js

--- src/index.js
+++ src/index.js
@@ -3,31 +3,20 @@ import ReactDOM from 'react-dom';
 import './Scss/index.scss';
 import App from './components/App/App';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import thunk from "redux-thunk";
-import rootReducer from "./reducers/index";
-import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import { rootReducer } from './reducers/index';
+import { Provider } from 'react-redux';
 
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
+const router = (
+	<Provider store={store}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>{' '}
+	</Provider>
 );
 
-ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById("root")
-);
-
-// const router = (
-//   <BrowserRouter>
-//   <App />
-//   </BrowserRouter>
-// )
-
-// ReactDOM.render(router, document.getElementById('root'));
+ReactDOM.render(router, document.getElementById('root'));
