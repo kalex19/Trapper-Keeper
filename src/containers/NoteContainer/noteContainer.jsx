7eb9ce608688dd3fca8eb7a3aaf1a4368f9a17c9
Saad Baradan <saadbaradan@gmail.com>
2019-05-27 17:44:55 -0600

    Fetch notes from back end and display on dom

--- src/containers/NoteContainer/noteContainer.jsx
+++ src/containers/NoteContainer/noteContainer.jsx
@@ -6,6 +6,7 @@ import Note from '../Note/note';
 import NotePopUp from '../NotePopUp/notepopup';
 import { getNotes } from '../../Util/thunks/getNotes';
 import { addNote } from '../../Util/thunks/addNote';
+import { deleteNote, deleteTask } from '../../actions';
 
 export class NoteContainer extends Component {
   constructor(props){
@@ -15,15 +16,21 @@ export class NoteContainer extends Component {
     }
   }
 
-  componentDidMount = async () => {
-    const response = await fetch('http://localhost:3001/api/v1/notes')
-    const result = await response.json()
-    this.setState({notes: result})
+  componentDidMount() {
+   fetch('http://localhost:3001/api/v1/notes')
+   .then(response => response.json())
+   .then(notes => this.setState({notes: notes}));
   }
+
+  deleteNote = (id) => {
+    let notes = this.state.notes.filter(note => note.id !== id);
+    this.setState({notes})
+	}
 
 
   render() {
-    return (
+    let filterOutBackEndTestNotes = this.state.notes.filter(note => note.title !== 'test')
+    return ( 
     <main className="NoteContainer">
     <Route path='/notes/:id' render={ ({ match }) => {
 				const { id } = match.params
@@ -32,10 +39,24 @@ export class NoteContainer extends Component {
 					return <NotePopUp {...note} />
 				}
       }}/>
-      <Route exact path='/new-note' component={NotePopUp} />
-      {this.props.notes.map(note => {
+    <Route exact path='/new-note' component={NotePopUp} />
+    {this.props.notes.map(note => {
         return <Note {...note}/>
-      })}
+    })}
+    {filterOutBackEndTestNotes.map(note => {
+      return (<article className='Note' key={note.id}>
+                <h2>{note.title}</h2>
+                  {note.tasks.map(task => {
+                    return (<article className='task' key={task.id}>
+							                  {note.complete === true ? <input className='check-box' type="checkbox" checked/> : <input className='check-box' type="checkbox" />}
+                                <p>{task.task}</p>
+							                  <button type='button' className='delete-task'><i className="fas fa-minus"></i></button>
+						                  </article>
+                            )
+                    })}
+                  <button type='button' onClick={() => this.deleteNote(note.id)} className="delete"><i className="fas fa-trash-alt" /></button>
+						    </article>
+          )})}       
 		  <Link to='/new-note' className='add-note-button'>
 			  <button>Add Note</button>
 		  </Link>
@@ -57,6 +78,7 @@ export const mapStateToProps = (state) => ({
 export const mapDispatchToProps = dispatch => ({
   getNotes: (url) => dispatch(getNotes(url)),
   addNote: (obj) => dispatch(addNote(obj))
+  // noteToDelete: (obj) =>  dispatch(deleteNote(obj))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(NoteContainer);
